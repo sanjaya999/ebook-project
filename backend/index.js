c712b0fe5c23bb15b2a3b847549a9b94d2dc3fc1
Sanjaya <poudelsanjaya12@gmail.com>
2024-02-14 06:49:51 +0545

    updated file structure

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose"
+import express from "express"
+import dotenv from "dotenv"
+import { app } from "./App.js"
+
+dotenv.config({
+    path:"./.env"
+})
+
+
+
+const DB_NAME = "ebookproject"
+const connectDB = async()=>{
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`connected db host ${connectionInstance}`)
+    }
+    catch(error){
+        console.log("connection failed server.js",error)
+    }
+
+}
+
+
+
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`server is running at port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("mongodb conn failed",err);
+})
