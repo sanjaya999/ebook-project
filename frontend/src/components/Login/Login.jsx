0f21d59d7970d63cf2e20fcee7df31f3598c00b5
Sanjaya <poudelsanjaya12@gmail.com>
2024-02-18 17:04:24 +0545

    frontend login added

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -6,17 +6,18 @@ function Login() {
  
 
   return (
-    <div>
-        <h2>Login</h2>
-
+    <div className='middle'>
+       
+       
         <form action="" >
-            <div>
-                <label htmlFor="email" className='label'>Email</label>
+        <h2 >Login</h2>
+            <div className='field'> 
+                <label htmlFor="email" className='label'>Email</label> <br />
                 <input type="email" className='input' />
             </div>
 
-            <div>
-                <label htmlFor="password" className='label'>Password</label>
+            <div className='field'>
+                <label htmlFor="password" className='label'>Password</label> <br />
                 <input type="Password" className='input' />
             </div>
 
@@ -25,9 +26,14 @@ function Login() {
             </div>
 
 
+            <div className="register">
+            <h3>Dont have an Account ? </h3>  <Link className='a' to="/Register" >Register</Link>
+
+            </div>
+            
         </form>
 
-        <h1>Dont have an Account ? </h1>  <Link to="/Register" >Register</Link>
+       
 
     </div>
   )
