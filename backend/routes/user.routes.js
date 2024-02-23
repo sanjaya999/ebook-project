bc84d4f7fc1b9abfb73900ec2ce73a920af9da66
Sanjaya <poudelsanjaya12@gmail.com>
2024-02-23 08:34:52 +0545

    logout routing added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { registerUser,loginUser } from "../controllers/user.controller.js";
-
+import { registerUser,loginUser,logoutUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -13,6 +13,7 @@ router.route("/register").post(upload.fields([
 ]),
 registerUser)
 router.route("/login").post(loginUser)
+router.route("/logout").post( verifyJWT , logoutUser)
 
 
 
