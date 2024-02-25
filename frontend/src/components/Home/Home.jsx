8edfbae4b2d1bf2fb245758cbca774e995e6eea8
Sanjaya <poudelsanjaya12@gmail.com>
2024-02-25 17:50:22 +0545

    Home added

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Home.css"
+import book from "../book.png"
 
 function Home() {
   return (
-    <div>
-        <h1>home</h1>
-    </div>
+   <>
+   <div className="logo"><img className="image"  src={book} alt="" srcset="" /></div>
+   
+
+   <div className="we">
+    <p className='aboutus'> 
+    We are world leading book room 
+    </p>
+   </div>
+
+   <div className="homelog">
+    <p className="sign">
+      <Link className="log" to="/Login">Login!</Link> to join us and show your writing!
+    </p>
+   </div>
+   </>
   )
 }
 
