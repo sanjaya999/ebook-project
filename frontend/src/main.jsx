11825bf6c1e0256f88f5a0c1364f60e9dec07667
Sanjaya <poudelsanjaya12@gmail.com>
2024-03-02 06:14:46 +0545

    alignment adjusted

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import  ReactDOM  from 'react-dom/client'
 
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+//import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import UserContextProvider from './Context/UserContext.jsx'
@@ -13,7 +13,7 @@ import UserContextProvider from './Context/UserContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserContextProvider>
+  <UserContextProvider>
    
   <App />
   </UserContextProvider>
