5bea18d9009c0c428de2207cd6624b72d6cde4b0
Sanjaya <poudelsanjaya12@gmail.com>
2024-03-03 07:48:53 +0545

    user Detail route added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { registerUser,loginUser,logoutUser } from "../controllers/user.controller.js";
+import { registerUser,loginUser,logoutUser,userDetail } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -14,6 +14,7 @@ router.route("/register").post(upload.fields([
 registerUser)
 router.route("/login").post(loginUser)
 router.route("/logout").post( verifyJWT , logoutUser)
+router.route("/userId").post(userDetail)
 
 
 
