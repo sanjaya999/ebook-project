592af680de2ea445b81e1b709e5ad13015fe33b9
Sanjaya <poudelsanjaya12@gmail.com>
2024-03-04 15:54:14 +0545

    401 unauthorized

--- frontend/src/components/UserProfile/UserProfile.jsx
+++ frontend/src/components/UserProfile/UserProfile.jsx
@@ -3,16 +3,26 @@ import "./UserProfile.css"
 import axios from "axios"
 
 
-function UserProfile() {
-    
-  const token = window.localStorage.getItem("token")
 
-  const response  = axios.get("http://localhost:5000/api/v1/userDetail" , {
-    header : {
-      Authorization : `Bearer ${token}`
-    }
-  })
-  console.log(response.data)
+function UserProfile() {
+   
+  const token = window.localStorage.getItem("token")
+    try{ 
+      const headers = {
+        'Authorization': `Bearer ${token}`
+    };
+      const response  = axios.get("http://localhost:5000/api/v1/user/userId" ,{
+        headers: headers
+      
+      }
+    )
+          console.log(response.data)
+      }
+      catch(error){
+        console.log(error)
+
+}
+ 
 
   return (
     <div>
