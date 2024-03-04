f1eecb6e3cdb01760610d06972bd124f91a2689d
Sanjaya <poudelsanjaya12@gmail.com>
2024-03-04 17:38:05 +0545

    correction on jwt

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-import Jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { User } from "../models/user.model.js";
@@ -7,10 +7,11 @@ export const verifyJWT = asyncHandler(async(req,res,next)=>{
     try{
         const token = req.cookies?.accessToken || req.headers("Authorization")?.replace("Bearer", "")
 
+        console.log(token);
         if(!token){
             throw new ApiError(401 , "Unauthorized request")
         }
-        const decodedToken = Jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
+        const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
          const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
          if(!user){
             throw new ApiError(401 , "Invalid Access Token")
