dfd85b505e2e9e3edf7b4d33b50a686d46cd397f
Sanjaya <poudelsanjaya12@gmail.com>
2024-03-05 17:13:03 +0545

    fixed bearer

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -3,13 +3,17 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { User } from "../models/user.model.js";
 
+
+
+
 export const verifyJWT = asyncHandler(async(req,res,next)=>{
     try{
-        const token = req.cookies?.accessToken || req.headers("Authorization")?.replace("Bearer", "")
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
 
         console.log(token);
+
         if(!token){
-            throw new ApiError(401 , "Unauthorized request")
+            throw new ApiError(401 , "No TOken ")
         }
         const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
          const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
