4927329ae0ab818e38aca91689200d21cbd989d6
Sanjaya <poudelsanjaya12@gmail.com>
2024-03-10 09:07:15 +0545

    created new book model

---
+++ backend/models/book.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+
+const pdfSchema = new mongoose.Schema(
+    {
+  bookName: { 
+    type: String, 
+    required: true
+     },
+
+ description: { 
+    type: String, 
+    required: true 
+    },
+
+    bookImage :{
+        type : String,
+    },
+
+
+  bookFile: {
+     type: String, 
+     required: true 
+    },
+
+  uploadedBy: { 
+    type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true 
+    }
+}
+, { timestamps: true });
+
+ export const Book = mongoose.model('Book', pdfSchema);
+
