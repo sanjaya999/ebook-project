c1bdb2accee7a2a93bd0978b937f74fada57b8c5
Sanjaya <poudelsanjaya12@gmail.com>
2024-03-12 06:06:24 +0545

    updated unique classnames

--- frontend/src/components/Explore/Explore.jsx
+++ frontend/src/components/Explore/Explore.jsx
@@ -24,40 +24,37 @@ function Explore() {
 
   
   return (
-    <div className="book-list-container">
-  <h1 className="book-list-title">Book List</h1>
-  <ul className="book-list">
-    {books &&
-      books.map((book, index) => (
-        <li key={index} className="book-item">
-          <h3 className="book-title">{book.bookName}</h3>
-          <p className="book-description">Description: {book.description}</p>
-          <p className="book-uploaded-by">Uploaded by: {book.uploadedBy}</p>
-          <p className="book-created-at">Created at: {book.createdAt}</p>
-          {/* Add more details you want to display */}
-          <div className="book-image-container">
-            <img
-              className="book-image"
-              src={book.bookImage}
-              alt={book.bookName}
-            />
-            {/* Assuming bookImage is the URL to the book's image */}
-          </div>
-          <div className="book-download-link">
-            <a
-              href={book.bookFile}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="book-download-btn"
-            >
-              Download Book
-            </a>
-            {/* Assuming bookFile is the URL to the book's PDF file */}
-          </div>
-        </li>
-      ))}
-  </ul>
-</div>
+    <div className="app-explore-book-list-container">
+    <h1 className="app-explore-book-list-title">Book List</h1>
+    <ul className="app-explore-book-list">
+      {books &&
+        books.map((book, index) => (
+          <li key={index} className="app-explore-book-item">
+            <h3 className="app-explore-book-title">{book.bookName}</h3>
+            <p className="app-explore-book-description">Description: {book.description}</p>
+            <p className="app-explore-book-uploaded-by">Uploaded by: {book.uploadedBy}</p>
+            <p className="app-explore-book-created-at">Created at: {book.createdAt}</p>
+            <div className="app-explore-book-image-container">
+              <img
+                className="app-explore-book-image"
+                src={book.bookImage}
+                alt={book.bookName}
+              />
+            </div>
+            <div className="app-explore-book-download-link">
+              <a
+                href={book.bookFile}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="app-explore-book-download-btn"
+              >
+                Download Book
+              </a>
+            </div>
+          </li>
+        ))}
+    </ul>
+  </div>
   )
 };
 
