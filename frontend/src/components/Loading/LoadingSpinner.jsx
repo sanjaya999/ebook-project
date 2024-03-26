5ea7334e99a4de4199344aa59d04d64253a5d54d
Sanjaya <poudelsanjaya12@gmail.com>
2024-03-26 10:15:19 +0545

    added loading spinner

---
+++ frontend/src/components/Loading/LoadingSpinner.jsx
@@ -0,0 +1,13 @@
+// LoadingSpinner.js
+import React from 'react';
+import './loading.css';
+
+const LoadingSpinner = () => {
+  return (
+    <div className="loading-spinner">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
