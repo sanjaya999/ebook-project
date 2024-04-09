958d19dc005d09f156f34585d0a11280277bb08d
Sanjaya <poudelsanjaya12@gmail.com>
2024-04-09 09:48:54 +0545

    cleaner

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -16,7 +16,9 @@ router.route("/register").post(upload.fields([
 ]),
 registerUser)
 router.route("/login").post(loginUser)
+
 router.route("/logout").post( verifyJWT , logoutUser)
+
 router.route("/userId").get(verifyJWT , userDetail)
 router.route("/upload").post(verifyJWT, upload.fields([
     {
@@ -33,6 +35,7 @@ router.route("/upload").post(verifyJWT, upload.fields([
 router.route("/explore").get(fetchBook)
 router.route("/search").get(search)
 router.route("/topPicks").get(topPicks)
+
 router.route("/adminBooks").get( verifyJWT , adminFetchBooks)
 router.route("/adminUsers").get(verifyJWT , adminFetchUsers)
 router.route("/adminDeleteUser").delete(verifyJWT,deleteUser)
