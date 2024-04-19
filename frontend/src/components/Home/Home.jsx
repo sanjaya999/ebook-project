58ab15b7dc598dee6e47dfd004daba89b04c9452
Sanjaya <poudelsanjaya12@gmail.com>
2024-04-19 10:56:07 +0545

    managed logged in / not loggedin

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,98 +1,125 @@
-import React ,{useState} from 'react'
-import { Link } from 'react-router-dom'
-import "./Home.css"
-import book from "../book.png"
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+import book from '../book.png';
 import axios from 'axios';
 import LoadingSpinner from '../Loading/LoadingSpinner';
 
-
 function Home() {
-
   const [searchTerm, setSearchTerm] = useState('');
   const [books, setBooks] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
+  const isLoggedIn = localStorage.getItem('loggedIn');
+  const accessToken = localStorage.getItem('token');
 
   const handleSearch = async () => {
     setIsLoading(true);
 
     try {
-      const response = await axios.get(`http://localhost:5000/api/v1/user/search?searchTerm=${searchTerm}`);
-      setBooks(response.data.data);
-      console.log(response.data.data)
-      setIsLoading(false);
+      let response;
+      if (isLoggedIn === 'true') {
+        const config = {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        };
+        response = await axios.get(
+          `http://localhost:5000/api/v1/user/search?searchTerm=${searchTerm}`,
+          config
+        );
+      } else {
+        response = await axios.get(
+          `http://localhost:5000/api/v1/user/search?searchTerm=${searchTerm}`
+        );
+      }
 
+      setBooks(response.data.data);
+      setIsLoading(false);
     } catch (err) {
       console.error(err);
+      setIsLoading(false);
     }
-
-  }
-
-
-
+  };
 
   return (
-   <>
-   <div className="logo"><img className="image"  src={book} alt="" srcset="" /></div>
-   
+    <>
+      <div className="logo">
+        <img className="image" src={book} alt="" />
+      </div>
 
-   <div className="we">
-    <p className='aboutus'> 
-    We are world leading book room 
-    </p>
-   </div>
+      <div className="we">
+        <p className="aboutus">We are world leading book room</p>
+      </div>
 
-   <div className="homelog">
-    <p className="sign">
-      <Link className="log" to="/Login">Login!</Link> to join us and show your writing!
-    </p>
-   </div>
+      <div className="homelog">
+        <p className="sign">
+          <Link className="log" to="/Login">
+            Login!
+          </Link>{' '}
+          to join us and show your writing!
+        </p>
+      </div>
 
+      <div className="search">
+        <input
+          type="text"
+          className="searchbook"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search For any books , articles , papers"
+        />
+        <button onClick={handleSearch} className="SButton">
+          Search
+        </button>
 
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <>
+            {Array.isArray(books) && books.length > 0 ? (
+              <ul className="book-list">
+                {books.map((book) => (
+                  <li key={book._id} className="book-item">
+                    <div className="book-image-container">
+                      <img
+                        className="book-image"
+                        src={book.bookImage}
+                        alt={book.bookName}
+                      />
+                    </div>
+                    <div className="book-details">
+                      <h3 className="book-title">{book.bookName}</h3>
+                      <p className="book-description">{book.description}</p>
+                      {isLoggedIn === 'true' && (
+                        <a
+                          className="book-download"
+                          href={book.bookFile}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          download=""
+                        >
+                          Download Book
+                        </a>
+                      )}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>No books found</p>
+            )}
+          </>
+        )}
+      </div>
 
-   <div className="search">
-    <input type="text" className='searchbook'  
-     value={searchTerm}
-     onChange={(e) => setSearchTerm(e.target.value)}
-    placeholder='Search For any books , articles , papers' />
-    <button  onClick={handleSearch} className='SButton'>Search</button>
-
-
-
-    {isLoading ? (
-        <LoadingSpinner />
-      ) : ( <>
-    {Array.isArray(books) && books.length > 0 ? (
-  <ul className="book-list">
-    {books.map((book) => (
-      <li key={book._id} className="book-item">
-        <div className="book-image-container">
-          <img className="book-image" src={book.bookImage} alt={book.bookName} />
-        </div>
-        <div className="book-details">
-          <h3 className="book-title">{book.bookName}</h3>
-          <a className="book-download" href={book.bookFile} download>
-            Download Book
-          </a>
-        </div>
-      </li>
-    ))}
-  </ul>
-) : (
-  <p>No books found</p>
-)}</>)}
-
-
-   </div>
-
-
-   <div className="section">
-    <Link to="/top" className='Top100'>Top 100</Link>
-    <div className="request">Request Book</div>
- 
-   </div>
-   </>
-  )
+      <div className="section">
+        <Link to="/top" className="Top100">
+          Top 100
+        </Link>
+        <div className="request">Request Book</div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
